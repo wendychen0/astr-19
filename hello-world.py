1f6f40b83ee0b9321ff09894032fe8eb6daf6728
Wendy Chen <50306459+wendychen0@users.noreply.github.com>
2022-03-31 11:01:29 -0700

    Added a hello world program
    This repo is now less boring

---
+++ hello-world.py
@@ -0,0 +1,16 @@
+#This program will write Hello World!
+
+#This function tells the computer
+#to print "Hello World!"
+def printHelloWorld():
+	print("Hello World!")
+
+#This defines our main() function
+#for our program
+def main():
+	printHelloWorld()
+
+#When we run the program this 
+#executes first
+if __name__=="__main__":
+	main()
