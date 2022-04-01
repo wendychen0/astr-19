f0157e4d5304e2a690d1e1fb6112c5735ee8938a
Wendy Chen <50306459+wendychen0@users.noreply.github.com>
2022-04-01 11:23:14 -0700

    Added a Python program about me

---
+++ session1.py
@@ -0,0 +1,16 @@
+#This program will write an intro about me!
+
+def printIntro():
+	print("My name is Wendy Chen, my pronouns are she/her.")
+	print("My favorite movies are Divergent and Jumanji:Welcome to the Jungle.") 
+	print("My favorite food is pasta especially with pesto.")
+
+#This defines our main() function
+#for our program
+def main():
+	printIntro()
+
+#When we run the program this 
+#executes first
+if __name__=="__main__":
+	main()
